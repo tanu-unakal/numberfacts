649c3f741d4f058d61c447eb91552f25bdd97d9b
Tanushree Unakal <tanu.unakal@berkeley.edu>
2020-06-15 12:47:52 +0400

    AAAHHH

---
+++ build/precache-manifest.e73cda03a09b5f1eb04ddcb99e566a5a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1007bdb8e366305688cbdb8accdaa26a",
+    "url": "/numberfacts/index.html"
+  },
+  {
+    "revision": "8a459a3a41a662939240",
+    "url": "/numberfacts/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "c31cd04a60019c7ad3bf",
+    "url": "/numberfacts/static/css/main.f883f95e.chunk.css"
+  },
+  {
+    "revision": "8a459a3a41a662939240",
+    "url": "/numberfacts/static/js/2.c958538f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/numberfacts/static/js/2.c958538f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c31cd04a60019c7ad3bf",
+    "url": "/numberfacts/static/js/main.1f8455a3.chunk.js"
+  },
+  {
+    "revision": "abed33b2522b8e164ed5",
+    "url": "/numberfacts/static/js/runtime-main.46ab0923.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/numberfacts/static/media/logo.5d5d9eef.svg"
+  }
+]);
